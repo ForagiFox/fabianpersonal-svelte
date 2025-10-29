5fb2389a487b16fd91f25e9f33e5dae140959dec
KnoyanMitsu <yanappuowo@yandex.com>
2025-10-29 12:57:39 +0700

    fix redirect

--- src/view/title.svelte
+++ src/view/title.svelte
@@ -1,6 +1,7 @@
 <script>
   import { IoArrowDownSharp } from 'svelte-icons-pack/io';
 	import { Icon } from 'svelte-icons-pack';
+	import { redirect } from '@sveltejs/kit';
 </script>
 
 <div class="lg:snap-start h-screen relative">
@@ -8,9 +9,9 @@
 				<h1 class="lg:text-7xl text-3xl font-bold text-black">Knoyan Mitsu</h1>
 				<h1 class="lg:text-4xl text-2xl font-normal text-black">Personal Website</h1>
 				<a
-					href=http://localhost:5173/cv
+					href='/cv'
 					class="bg-black rounded-full mt-3 w-64 text-center h-10 flex items-center justify-center gap-2 text-white font-semibold hover:bg-gray-800 transition-colors"
-				>Show CV ATS</a>	
+				>Show CV ATS</a>
 			</div>
 			<!-- Ikon panah di bagian bawah -->
 			<div
