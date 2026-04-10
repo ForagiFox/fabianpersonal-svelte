8fba8c987e377aa587d79f9e0e7c5197a3b36cd1
Fabian Ananda Merdana <yanappuowo@yandex.com>
2026-04-10 22:53:31 +0700

    modify contact

--- src/view/contact.svelte
+++ src/view/contact.svelte
@@ -6,12 +6,16 @@
 <div class="bg-white shadow-xl w-full lg:w-1/2 mt-9 lg:mt-0 rounded-md">
 	<div class="py-4 px-7">
 		<h1 class="text-3xl font-bold mb-5 text-black">
-			Connect Me
+			Contact Me
 		</h1>
 		<div class="mb-5">
                 <p class="text-black font-semibold mb-1">Email</p>
                 <p class="text-black font-semibold">fabianananda0912@gmail.com</p>
-            </div>
+        </div>
+		<div class="mb-5">
+			<p class="text-black font-semibold mb-1">Whatsapp</p>
+            <p class="text-black font-semibold">+62 81357803479</p>
+		</div>
 		<p class="text-black font-semibold mb-1">Connect me</p>
 		<div class="flex gap-3">
 			<a href="https://github.com/KnoyanMitsu"><Icon size="40" src={BsGithub} /></a>
